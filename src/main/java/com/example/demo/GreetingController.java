675c9b7b9c0bcffe33086025a64a1b6ce35881ea
davor <davor.rajic@consulteer.com>
2024-07-04 23:35:10 +0200

    ff

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,10 +14,18 @@ import java.util.Locale;
 public class GreetingController {
 
     private final TenantMessageSource tenantMessageSource;
+    private final ApplicationContext applicationContext;
 
-    @GetMapping("/greet")
-    public String greet(@RequestHeader("X-Tenant-ID") String tenantId) {
-        MessageSource messageSource = tenantMessageSource.getMessageSource(tenantId);
+//    @GetMapping("/greet")
+//    public String greet(@RequestHeader("X-Tenant-ID") String tenantId) {
+//        MessageSource messageSource = tenantMessageSource.getMessageSource(tenantId);
+//        return messageSource.getMessage("greeting", null, Locale.ENGLISH);
+//    }
+
+
+    @GetMapping("/greet/new")
+    public String greetNew(@RequestHeader("X-Tenant-ID") String tenantId) {
+        MessageSource messageSource = (MessageSource) applicationContext.getBean(tenantId + "_messageSource");
         return messageSource.getMessage("greeting", null, Locale.ENGLISH);
     }
 }
--- src/main/java/com/example/demo/TenantMessageSource.java
+++ src/main/java/com/example/demo/TenantMessageSource.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +11,31 @@ import java.util.concurrent.ConcurrentMap;
 @Component
 public class TenantMessageSource {
 
-    private final ConcurrentMap<String, MessageSource> messageSourceCache = new ConcurrentHashMap<>();
+//    private final ConcurrentMap<String, MessageSource> messageSourceCache = new ConcurrentHashMap<>();
+//
+//    public MessageSource getMessageSource(String tenantId) {
+//        return messageSourceCache.computeIfAbsent(tenantId, this::createMessageSource);
+//    }
+//
+//    private MessageSource createMessageSource(String tenantId) {
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setBasename("classpath:/i18n/" + tenantId + "/messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        return messageSource;
+//    }
 
-    public MessageSource getMessageSource(String tenantId) {
-        return messageSourceCache.computeIfAbsent(tenantId, this::createMessageSource);
+    @Bean("tenant1_messageSource")
+        private MessageSource firstTenant() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/i18n/tenant1/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 
-    private MessageSource createMessageSource(String tenantId) {
+    @Bean("tenant2_messageSource")
+    private MessageSource secondTenant() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:/i18n/" + tenantId + "/messages");
+        messageSource.setBasename("classpath:/i18n/tenant2/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
